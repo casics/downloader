827094b8be3cb42093da980ed5a1eed37ea91d08
Michael Hucka <mhucka@caltech.edu>
2017-11-07 21:34:33 -0800

    Update file headers to new format
    I'm settling on a new scheme for file headers for all files that are
    not Python module files.  The new scheme uses a comment block and not
    documentation strings.  For Python modules, I'm continuing to use a
    documentation string under the assumption that this is accessible
    within Python and Pythonic tools.

--- downloader/version.py
+++ downloader/version.py
@@ -1,18 +1,15 @@
-#
+# =============================================================================
 # @file    version.py
 # @brief   CASICS Downloader version info
-# @author  Michael Hucka
-#
-# <!---------------------------------------------------------------------------
-# Copyright (C) 2015 by the California Institute of Technology.
-# This software is part of CASICS, the Comprehensive and Automated Software
-# Inventory Creation System.  For more information, visit http://casics.org.
-# ------------------------------------------------------------------------- -->
+# @author  Michael Hucka <mhucka@caltech.edu>
+# @license Please see the file named LICENSE in the project directory
+# @website https://github.com/casics/downloader
+# =============================================================================
 
-__version__      = '1.0.0'
-__title__        = 'CASICS Downloader'
-__url__          = 'https://github.com/casics/downloader'
-__author__       = 'Michael Hucka <mhucka@caltech.edu>'
-__email__        = 'casics-team@googlegroups.com'
-__license__      = 'GNU General Public License, version 3.0'
-__copyright__    = 'Copyright (C) 2015 by the California Institute of Technology'
+__version__   = '1.0.0'
+__title__     = 'CASICS Downloader'
+__url__       = 'https://github.com/casics/downloader'
+__author__    = 'Michael Hucka <mhucka@caltech.edu>'
+__email__     = 'casics-team@googlegroups.com'
+__license__   = 'GNU General Public License, version 3.0'
+__copyright__ = 'Copyright (C) 2015-2017 by the California Institute of Technology'
